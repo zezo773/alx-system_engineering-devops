c3d0e05e2b2d147798c3030efd146a086460b5cd
zezo773 <ziadammar384@gmail.com>
2026-01-07 15:00:51 +0200

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
+# Install an especific version of flask (2.1.0)
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'flask':
+    provider    => 'pip3',
+    ensure  => '2.1.0'
 }
